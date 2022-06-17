b0ae531c5cea4870b82c81bb7472357a5f4e69b8
IlhamHafsyah <ilhamhafsyah15@gmail.com>
2022-06-17 09:21:12 +0700

    user routes prepared

--- server/routes/user_router.go
+++ server/routes/user_router.go
@@ -1,7 +1,10 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+    "gin-mongo-api/controllers"
+    "github.com/gin-gonic/gin"
+)
 
 func UserRoute(router *gin.Engine)  {
-    //All routes related to users comes here
+    router.POST("/user", controllers.CreateUser())
 }
