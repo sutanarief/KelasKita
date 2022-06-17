17f102b67493f21f00da1b3ae67bf21983255b97
IlhamHafsyah <ilhamhafsyah15@gmail.com>
2022-06-17 09:23:43 +0700

    get user restful prepared

--- server/controllers/user_controller.go
+++ server/controllers/user_controller.go
@@ -12,6 +12,7 @@ import (
     "github.com/go-playground/validator/v10"
     "go.mongodb.org/mongo-driver/bson/primitive"
     "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "users")
@@ -49,5 +50,24 @@ func CreateUser() gin.HandlerFunc {
         }
 
         c.JSON(http.StatusCreated, responses.UserResponse{Status: http.StatusCreated, Message: "success", Data: map[string]interface{}{"data": result}})
+    }
+}
+
+func GetAUser() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+        userId := c.Param("userId")
+        var user models.User
+        defer cancel()
+
+        objId, _ := primitive.ObjectIDFromHex(userId)
+
+        err := userCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&user)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+            return
+        }
+
+        c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": user}})
     }
 }
--- server/routes/user_router.go
+++ server/routes/user_router.go
@@ -7,4 +7,5 @@ import (
 
 func UserRoute(router *gin.Engine)  {
     router.POST("/user", controllers.CreateUser())
+	router.GET("/user/:userId", controllers.GetAUser())
 }
